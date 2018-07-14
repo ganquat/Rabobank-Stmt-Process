2fdd000ad53deff198200063b878a5916078ff22
Ganesh <ganquat@gmail.com>
2018-07-14 12:43:41 +0530

    test case upload

--- src/main/java/com/rabobank/statement/service/CustStmtService.java
+++ src/main/java/com/rabobank/statement/service/CustStmtService.java
@@ -1,10 +1,10 @@
 package com.rabobank.statement.service;
 
-import com.rabobank.statement.constants.CustStmtConstant;
-import com.rabobank.statement.factory.StmtProcessorFactory;
 import com.rabobank.statement.interfaces.ICustStmtInterface;
 import com.rabobank.statement.model.CustStmtModel;
+import com.rabobank.statement.processor.StmtProcessorFactory;
 import com.rabobank.statement.processor.XMLStmtProcessor;
+import com.rabobank.statement.validator.CustStmtValidator;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +18,7 @@ import java.util.List;
  * Service class for processing customer statement
  */
 @Service
-public class CustStmtService implements CustStmtConstant {
+public class CustStmtService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustStmtService.class);
 
@@ -28,6 +28,9 @@ public class CustStmtService implements CustStmtConstant {
     @Autowired
     private StmtProcessorFactory stmtProcessorFactory;
 
+    @Autowired
+    private CustStmtValidator custStmtValidator;
+
     /*
     *   This method validates the customer statement records and updates the failed record in list
     *   @param custStmtModel
@@ -36,7 +39,7 @@ public class CustStmtService implements CustStmtConstant {
         LOGGER.info("Inside customer statement validation method");
 
         List<CustStmtModel> custStmtModelList = convertFileDataToCustStmtModel(file);
-        List<CustStmtModel> validatedStmtModelList = checkForInvalidTransaction(custStmtModelList);
+        List<CustStmtModel> validatedStmtModelList = custStmtValidator.checkForInvalidTransaction(custStmtModelList);
 
         return validatedStmtModelList;
     }
@@ -49,30 +52,6 @@ public class CustStmtService implements CustStmtConstant {
                 FilenameUtils.getExtension(file.getOriginalFilename()));
 
         List<CustStmtModel> custStmtModelList = CustStmtObject.process(file);
-        return custStmtModelList;
-    }
-
-    /*
-     * This method check for invalid transaction from the list of customer statement model list
-     */
-    private List<CustStmtModel> checkForInvalidTransaction(List<CustStmtModel> custStmtModelList){
-
-        for (CustStmtModel custModel :  custStmtModelList) {
-            //Calculate and check the end balance value
-            if (Math.round((custModel.getStartBalance() + custModel.getMutation()) *
-                    ROUND_TWO_DECIMAL)/ROUND_TWO_DECIMAL == custModel.getEndBalance()) {
-                // If end balance is correct, loop through all values and check duplicate ref no
-                for (CustStmtModel custModel1 : custStmtModelList) {
-                    if ((custModel.getRefernceNo().equals(custModel1.getRefernceNo())
-                            && (!custModel.equals(custModel1))))
-                        //duplicate reference no
-                        custModel.setFailedRecord(true);
-                }
-            }
-            else
-                //end balance is not correct
-                custModel.setFailedRecord(true);
-        }
         return custStmtModelList;
     }
 }
